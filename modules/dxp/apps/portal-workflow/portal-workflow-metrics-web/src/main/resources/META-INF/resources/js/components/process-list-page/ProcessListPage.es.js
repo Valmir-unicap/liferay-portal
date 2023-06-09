ef0aedafbae93eaded784a95c2d8b75d844b9f95
valmir junior <valmiralvesjr2000@hotmail.com>
2023-06-09 14:57:39 -0300

    testando

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-list-page/ProcessListPage.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-list-page/ProcessListPage.es.js
@@ -51,7 +51,7 @@ function ProcessListPage({history, query, routeParams}) {
 		history.replace(`/processes/20/1/overdueInstanceCount:desc`);
 	}
 
-	usePageTitle(Liferay.Language.get('metrics'));
+	usePageTitle(Liferay.Language.get('add-product'));
 
 	const {page, pageSize, sort} = routeParams;
 	const {search = ''} = parse(query);
