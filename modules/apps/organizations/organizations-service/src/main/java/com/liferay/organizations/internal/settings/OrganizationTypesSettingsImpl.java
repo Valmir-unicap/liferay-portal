bb7aab7eaf16fa5b912f99370c99451c9a32ffee
valmir junior <valmiralvesjr2000@hotmail.com>
2023-11-21 14:24:04 -0300

    LPS-198452 Create inner class OrganizationTypeConfigurationManagedServiceFactory with implements ManagedServiceFactory, apply to usage, and prepare for next

--- modules/apps/organizations/organizations-service/src/main/java/com/liferay/organizations/internal/settings/OrganizationTypesSettingsImpl.java
+++ modules/apps/organizations/organizations-service/src/main/java/com/liferay/organizations/internal/settings/OrganizationTypesSettingsImpl.java
@@ -5,16 +5,27 @@
 
 package com.liferay.organizations.internal.settings;
 
+import com.liferay.organizations.internal.configuration.OrganizationTypeConfiguration;
 import com.liferay.organizations.internal.configuration.OrganizationTypeConfigurationWrapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.users.admin.kernel.organization.types.OrganizationTypesSettings;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedServiceFactory;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -84,8 +95,53 @@ public class OrganizationTypesSettingsImpl
 		return organizationTypeConfigurationWrapper.isRootable();
 	}
 
+	public class OrganizationTypeConfigurationManagedServiceFactory
+		implements ManagedServiceFactory {
+
+		@Override
+		public void deleted(String pid) {
+			String organizationTypeConfiguration =
+				_configurationPidOrganizationTypeConfigurationName.remove(pid);
+
+			_organizationTypeConfigurationNameOrganizationTypeConfiguration.
+				remove(organizationTypeConfiguration);
+		}
+
+		@Override
+		public String getName() {
+			return "com.liferay.organizations.internal.configuration." +
+				"OrganizationTypeConfiguration";
+		}
+
+		@Override
+		public void updated(String pid, Dictionary<String, ?> dictionary)
+			throws ConfigurationException {
+
+			OrganizationTypeConfiguration organizationTypeConfiguration =
+				ConfigurableUtil.createConfigurable(
+					OrganizationTypeConfiguration.class, dictionary);
+
+			_configurationPidOrganizationTypeConfigurationName.put(
+				pid, organizationTypeConfiguration.name());
+			_organizationTypeConfigurationNameOrganizationTypeConfiguration.put(
+				organizationTypeConfiguration.name(),
+				organizationTypeConfiguration);
+		}
+
+	}
+
 	@Activate
 	protected void activate(BundleContext bundleContext) {
+		_managedServiceFactoryServiceRegistration =
+			bundleContext.registerService(
+				ManagedServiceFactory.class,
+				new OrganizationTypeConfigurationManagedServiceFactory(),
+				HashMapDictionaryBuilder.put(
+					Constants.SERVICE_PID,
+					"com.liferay.organizations.internal.configuration." +
+						"OrganizationTypeConfiguration"
+				).build());
+
 		_organizationTypeConfigurationWrapperServiceTrackerMap =
 			ServiceTrackerMapFactory.openSingleValueMap(
 				bundleContext, OrganizationTypeConfigurationWrapper.class, null,
@@ -98,6 +154,7 @@ public class OrganizationTypesSettingsImpl
 
 	@Deactivate
 	protected void deactivate() {
+		_managedServiceFactoryServiceRegistration.unregister();
 		_organizationTypeConfigurationWrapperServiceTrackerMap.close();
 	}
 
@@ -119,6 +176,13 @@ public class OrganizationTypesSettingsImpl
 	private static final Log _log = LogFactoryUtil.getLog(
 		OrganizationTypesSettingsImpl.class);
 
+	private final Map<String, String>
+		_configurationPidOrganizationTypeConfigurationName = new HashMap<>();
+	private ServiceRegistration<ManagedServiceFactory>
+		_managedServiceFactoryServiceRegistration;
+	private final Map<String, OrganizationTypeConfiguration>
+		_organizationTypeConfigurationNameOrganizationTypeConfiguration =
+			new HashMap<>();
 	private ServiceTrackerMap<String, OrganizationTypeConfigurationWrapper>
 		_organizationTypeConfigurationWrapperServiceTrackerMap;
 
