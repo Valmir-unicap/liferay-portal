d7468cdd873c990ffcccf6ad46a04c224e560230
Valmir Junior <valmir.junior@liferay.com>
2024-06-03 18:20:27 -0300

    LPD-27027 Apply Singleton in class CommerceInventoryWarehouseCityComparator, apply to usages
    # breaking
    
    ## What modules/apps/commerce/commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/util/comparator/CommerceInventoryWarehouseCityComparator.java
    
    the class is now a singleton
    
    ## Why
    
    this change was made to improve the performance of the querys that uses this comparator
    
    ----

--- modules/apps/commerce/commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/util/comparator/CommerceInventoryWarehouseCityComparator.java
+++ modules/apps/commerce/commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/util/comparator/CommerceInventoryWarehouseCityComparator.java
@@ -21,8 +21,14 @@ public class CommerceInventoryWarehouseCityComparator
 
 	public static final String[] ORDER_BY_FIELDS = {"city"};
 
-	public CommerceInventoryWarehouseCityComparator(boolean ascending) {
-		_ascending = ascending;
+	public static CommerceInventoryWarehouseCityComparator getInstance(
+		boolean ascending) {
+
+		if (ascending) {
+			return _INSTANCE_ASCENDING;
+		}
+
+		return _INSTANCE_DESCENDING;
 	}
 
 	@Override
@@ -62,6 +68,18 @@ public class CommerceInventoryWarehouseCityComparator
 	public boolean isAscending() {
 		return _ascending;
 	}
+
+	private CommerceInventoryWarehouseCityComparator(boolean ascending) {
+		_ascending = ascending;
+	}
+
+	private static final CommerceInventoryWarehouseCityComparator
+		_INSTANCE_ASCENDING = new CommerceInventoryWarehouseCityComparator(
+			true);
+
+	private static final CommerceInventoryWarehouseCityComparator
+		_INSTANCE_DESCENDING = new CommerceInventoryWarehouseCityComparator(
+			false);
 
 	private final boolean _ascending;
 
