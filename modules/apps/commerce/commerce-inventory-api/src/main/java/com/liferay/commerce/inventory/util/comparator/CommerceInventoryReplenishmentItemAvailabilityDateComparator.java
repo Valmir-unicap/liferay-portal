4aebd76cdff4d7dad6331e86a3817f47e7e10a1a
Valmir Junior <valmir.junior@liferay.com>
2024-06-03 18:35:39 -0300

    LPD-27027 Apply Singleton in class CommerceInventoryReplenishmentItemAvailabilityDateComparator, apply to usages
    the class is now a singleton
    
    this change was made to improve the performance of the querys that uses this comparator
    
    ----

--- modules/apps/commerce/commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/util/comparator/CommerceInventoryReplenishmentItemAvailabilityDateComparator.java
+++ modules/apps/commerce/commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/util/comparator/CommerceInventoryReplenishmentItemAvailabilityDateComparator.java
@@ -21,10 +21,14 @@ public class CommerceInventoryReplenishmentItemAvailabilityDateComparator
 
 	public static final String[] ORDER_BY_FIELDS = {"availabilityDate"};
 
-	public CommerceInventoryReplenishmentItemAvailabilityDateComparator(
-		boolean ascending) {
+	public static CommerceInventoryReplenishmentItemAvailabilityDateComparator
+		getInstance(boolean ascending) {
 
-		_ascending = ascending;
+		if (ascending) {
+			return _INSTANCE_ASCENDING;
+		}
+
+		return _INSTANCE_DESCENDING;
 	}
 
 	@Override
@@ -62,6 +66,24 @@ public class CommerceInventoryReplenishmentItemAvailabilityDateComparator
 	public boolean isAscending() {
 		return _ascending;
 	}
+
+	private CommerceInventoryReplenishmentItemAvailabilityDateComparator(
+		boolean ascending) {
+
+		_ascending = ascending;
+	}
+
+	private static final
+		CommerceInventoryReplenishmentItemAvailabilityDateComparator
+			_INSTANCE_ASCENDING =
+				new CommerceInventoryReplenishmentItemAvailabilityDateComparator(
+					true);
+
+	private static final
+		CommerceInventoryReplenishmentItemAvailabilityDateComparator
+			_INSTANCE_DESCENDING =
+				new CommerceInventoryReplenishmentItemAvailabilityDateComparator(
+					false);
 
 	private final boolean _ascending;
 
