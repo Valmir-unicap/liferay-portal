1eb4bc98df50668d76b532fa2d2f64a20c60960c
Valmir Junior <valmir.junior@liferay.com>
2024-06-03 18:25:32 -0300

    LPD-27027 Apply Singleton in class CommerceInventoryWarehouseItemWarehouseNameComparator, apply to usages
    # breaking
    
    ## What modules/apps/commerce/commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/util/comparator/CommerceInventoryWarehouseItemWarehouseNameComparator.java
    
    the class is now a singleton
    
    ## Why
    
    this change was made to improve the performance of the querys that uses this comparator
    
    ----

--- modules/apps/commerce/commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/util/comparator/CommerceInventoryWarehouseItemWarehouseNameComparator.java
+++ modules/apps/commerce/commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/util/comparator/CommerceInventoryWarehouseItemWarehouseNameComparator.java
@@ -28,10 +28,14 @@ public class CommerceInventoryWarehouseItemWarehouseNameComparator
 		"CommerceInventoryWarehouse.name"
 	};
 
-	public CommerceInventoryWarehouseItemWarehouseNameComparator(
-		boolean ascending) {
+	public static CommerceInventoryWarehouseItemWarehouseNameComparator
+		getInstance(boolean ascending) {
 
-		_ascending = ascending;
+		if (ascending) {
+			return _INSTANCE_ASCENDING;
+		}
+
+		return _INSTANCE_DESCENDING;
 	}
 
 	@Override
@@ -81,6 +85,20 @@ public class CommerceInventoryWarehouseItemWarehouseNameComparator
 	public boolean isAscending() {
 		return _ascending;
 	}
+
+	private CommerceInventoryWarehouseItemWarehouseNameComparator(
+		boolean ascending) {
+
+		_ascending = ascending;
+	}
+
+	private static final CommerceInventoryWarehouseItemWarehouseNameComparator
+		_INSTANCE_ASCENDING =
+			new CommerceInventoryWarehouseItemWarehouseNameComparator(true);
+
+	private static final CommerceInventoryWarehouseItemWarehouseNameComparator
+		_INSTANCE_DESCENDING =
+			new CommerceInventoryWarehouseItemWarehouseNameComparator(false);
 
 	private final boolean _ascending;
 
